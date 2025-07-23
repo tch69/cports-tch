84327536ce09495bc55c973c6ef1513c4306ebda
tch69 <zerqkaes@memeware.net>
2025-07-23 09:31:46 +0700

    ibus-bamboo: bump

--- ibus-bamboo/template.py
+++ ibus-bamboo/template.py
@@ -7,8 +7,8 @@ makedepends = ["gtk+3-devel", "ibus-devel", "pkgconf-devel"]
 pkgdesc = "Vietnamese IME for IBus"
 license = "GPL-3.0-only"
 url = "https://github.com/BambooEngine/ibus-bamboo"
-source = f"https://github.com/BambooEngine/{pkgname}/archive/v{pkgver}-RC1.tar.gz"
-sha256 = "8ca197b5234a2a8c93ca9852376b8ba31cb29311fff26c53d6a1ffce538d1ef2"
+source = f"https://github.com/BambooEngine/{pkgname}/archive/v{pkgver}-RC6.tar.gz"
+sha256 = "610031553a033cde9f0c8bb08fd8578763611f24964566973997d7ae57a3c99e"
 # Program contains no tests
 options = ["!check"]
 
